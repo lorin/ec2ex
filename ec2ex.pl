fb02fcce91a7e213ae800dcc5d18e1faf2f38a74
Lorin Hochstein <lorin.hochstein@sendgrid.com>
2014-05-15 21:13:57 -0400

    instance ids

--- ec2ex.pl
+++ ec2ex.pl
@@ -1,5 +1,10 @@
 #!/usr/bin/perl
 #
+
+@chars = (0 .. 9, "a" .. "g");
+
 while (<>) {
+    $ec2id = join("", @chars[ map { rand @chars } ( 1 .. 8 ) ]);
+    s/i-[0-9a-g]{8}/i-$ec2id/g;
     print;
 }
